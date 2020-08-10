0dc6b9020dbfc24145755b18ade6831de8f6385d
Victor <vapoyan@kasa.host>
2020-08-10 21:26:10 +0200

    Cleaned Up navigation

--- app/src/main/java/app/chamich/PerfectusActivity.kt
+++ app/src/main/java/app/chamich/PerfectusActivity.kt
@@ -35,48 +35,53 @@ class PerfectusActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_perfectus)
 
-
         // Set application theme mode
         preferences.loadInt(KEY_PREFERENCE_THEME_MODE)?.let { mode ->
             AppCompatDelegate.setDefaultNightMode(mode)
         }
 
-        binding.appbar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem?.itemId) {
-                R.id.action_sign_out -> {
-                    authenticator.signOut()
-//                    navController.setGraph(R.navigation.settings_navigation)
-                }
-                R.id.action_settings -> {
-                    navController.navigate(R.id.navigation_settings)
-                    //navController.setGraph(R.navigation.settings_navigation)
-                }
-            }
-            return@setOnMenuItemClickListener true
-        }
-
         if (authenticator.isSignedIn()) {
-            binding.appbar.isVisible = true
-            binding.fabSearch.isVisible = true
-            // Call navController.setGraph(...) with providing correct graph.
+            navController.navigate(R.id.navigation_goals)
         }
 
+        addMenuItemClickListener()
         addDestinationChangeListener()
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     //region Private Function
 
+    private fun addMenuItemClickListener() {
+        binding.appbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem?.itemId) {
+                R.id.action_sign_out -> {
+                    authenticator.signOut()
+                    navController.navigate(R.id.navigation_authentication)
+                }
+                R.id.action_settings -> {
+                    navController.navigate(R.id.navigation_settings)
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
+    }
+
     private fun addDestinationChangeListener() {
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             when (destination.id) {
+                // In the case if the user is not signed in yet, the Action Bar,
+                // Bottom Application Bar and the Floating Action Button should be hidden.
                 R.id.destination_fragment_sign_in,
                 R.id.destination_fragment_sign_up,
                 R.id.destination_forgot_password -> {
-                    // Hides the Title and the Bottom Application Bar
                     supportActionBar?.hide()
                     binding.appbar.isVisible = false
                     binding.fabSearch.isVisible = false
+                }
+                R.id.destination_goals -> {
+                    supportActionBar?.show()
+                    binding.appbar.isVisible = true
+                    binding.fabSearch.isVisible = true
                 }
             }
 
@@ -90,9 +95,7 @@ class PerfectusActivity : AppCompatActivity(),
     //region SignInListener
 
     override fun onSignInCompleted(user: IUser) {
-        // Call navController.setGraph(...) with providing correct graph.
-        binding.appbar.isVisible = true
-        binding.fabSearch.isVisible = true
+        navController.navigate(R.id.navigation_goals)
     }
 
     //endregion
@@ -101,9 +104,7 @@ class PerfectusActivity : AppCompatActivity(),
     //region SignUpListener
 
     override fun onSignUpCompleted(user: IUser) {
-        // Call navController.setGraph(...) with providing correct graph.
-        binding.appbar.isVisible = true
-        binding.fabSearch.isVisible = true
+        navController.navigate(R.id.navigation_goals)
     }
 
     //endregion
---
+++ features/goals/src/main/java/app/chamich/feature/goals/ui/goals/GoalsFragment.kt
@@ -0,0 +1,18 @@
+package app.chamich.feature.goals.ui.goals
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import app.chamich.feature.goals.R
+
+class GoalsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.goals_fragment_goals, container, false)
+    }
+}
