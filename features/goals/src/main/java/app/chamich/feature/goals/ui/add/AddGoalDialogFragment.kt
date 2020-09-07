a8159fb5324b8b3b9d786a00ae27ab02a67893ea
Viktor Apoyan <48297488+ChamichApps@users.noreply.github.com>
2020-09-07 22:40:47 +0200

    Added measured in functionality (#12)
    Co-authored-by: Victor <vapoyan@kasa.host>

--- features/goals/src/main/java/app/chamich/feature/goals/ui/add/AddGoalDialogFragment.kt
+++ features/goals/src/main/java/app/chamich/feature/goals/ui/add/AddGoalDialogFragment.kt
@@ -7,11 +7,16 @@ package app.chamich.feature.goals.ui.add
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.clearFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import app.chamich.feature.goals.R
 import app.chamich.feature.goals.databinding.GoalsDialogFragmentAddGoalBinding
 import app.chamich.feature.goals.model.Goal
+import app.chamich.feature.goals.model.Measurement
+import app.chamich.feature.goals.ui.measuredin.MeasuredInBottomSheet
 import app.chamich.library.core.CoreDialogFragment
 import app.chamich.library.core.model.Status
 import dagger.hilt.android.AndroidEntryPoint
@@ -50,6 +55,7 @@ internal class AddGoalDialogFragment :
         super.onViewCreated(view, savedInstanceState)
 
         binding.fragment = this
+        binding.viewmodel = viewModel
 
         setupObservers()
     }
@@ -66,12 +72,33 @@ internal class AddGoalDialogFragment :
         viewModel.addGoal(
             Goal(
                 title = binding.editTextTitle.text.toString(),
-                measuredIn = 1,
+                measuredIn = viewModel.measurement.id,
                 totalEffort = 1,
                 progress = 1,
                 completeData = 1L,
                 category = 1,
                 color = 1
+            )
+        )
+    }
+
+    fun onMeasuredInClicked() {
+        // IMPROVEME: If bottom sheet is already shown, do not show it again.
+        // This can happen when user very fast clicks more then once on the label.
+
+        // Sets the fragment result listener
+        setFragmentResultListener(MeasuredInBottomSheet.KEY_RESULT_LISTENER) { key, bundle ->
+            if (MeasuredInBottomSheet.KEY_RESULT_LISTENER == key) {
+                viewModel.measurement =
+                    bundle.getSerializable(MeasuredInBottomSheet.KEY_RESULT_MEASUREMENT) as Measurement
+                binding.viewmodel = viewModel
+                clearFragmentResult(MeasuredInBottomSheet.KEY_RESULT_LISTENER)
+            }
+        }
+
+        findNavController().navigate(
+            R.id.destination_measured_in, bundleOf(
+                MeasuredInBottomSheet.KEY_SENT_MEASURED_ID to viewModel.measurement.id
             )
         )
     }
