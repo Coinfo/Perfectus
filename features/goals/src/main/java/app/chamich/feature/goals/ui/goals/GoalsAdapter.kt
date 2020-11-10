3f3c3b0cd13e09d34fc68bdbdecb2e6025ea8653
Viktor Apoyan <48297488+ChamichApps@users.noreply.github.com>
2020-11-10 21:22:15 +0100

    Added categories to the Goals list (#30)
    Signed-off-by: Viktor Apoyan <chamich.apps@gmail.com>

--- features/goals/src/main/java/app/chamich/feature/goals/ui/goals/GoalsAdapter.kt
+++ features/goals/src/main/java/app/chamich/feature/goals/ui/goals/GoalsAdapter.kt
@@ -10,34 +10,59 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import app.chamich.feature.goals.R
 import app.chamich.feature.goals.databinding.GoalsItemGoalBinding
+import app.chamich.feature.goals.databinding.GoalsItemHeaderBinding
+import app.chamich.feature.goals.model.Category
 import app.chamich.feature.goals.model.api.IGoal
 
 
 internal class GoalsAdapter(
     private val listener: (Long) -> Unit
-) : RecyclerView.Adapter<GoalsAdapter.GoalsViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private val goals: MutableList<IGoal> = mutableListOf()
+    private val items: MutableList<GoalUi> = mutableListOf()
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     //region Override Functions
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalsViewHolder =
-        GoalsViewHolder(
-            DataBindingUtil.inflate(
-                LayoutInflater.from(parent.context),
-                R.layout.goals_item_goal,
-                parent,
-                false
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when (viewType) {
+            VIEW_TYPE_GOAL -> GoalsViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.goals_item_goal,
+                    parent,
+                    false
+                )
             )
-        )
+            VIEW_TYPE_HEADER -> HeaderViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.goals_item_header,
+                    parent,
+                    false
+                )
+            )
+            else -> throw IllegalStateException("Unknown view type provided.")
+        }
 
-    override fun getItemCount() = goals.size
+    override fun getItemCount() = items.size
 
-    override fun onBindViewHolder(holder: GoalsViewHolder, position: Int) {
-        val goal = goals[position]
-        holder.binding.goal = goal
-        holder.binding.root.setOnClickListener { listener(goal.id) }
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is GoalsViewHolder -> {
+                val goal = items[position]
+                holder.binding.goal = goal
+                holder.binding.root.setOnClickListener { listener(goal.id) }
+            }
+            is HeaderViewHolder -> {
+                val header = items[position]
+                holder.binding.textViewTitle.setText(Category.valueOf(header.category).stringRes)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (items[position].isHeader) VIEW_TYPE_HEADER else VIEW_TYPE_GOAL
     }
 
     //endregion
@@ -47,8 +72,18 @@ internal class GoalsAdapter(
     //region Public Functions
 
     fun addGoals(goals: List<IGoal>) {
-        this.goals.clear()
-        this.goals.addAll(goals)
+        val categoriesMap = Category.asList().map { it.id to false }.toMap().toMutableMap()
+
+        this.items.clear()
+        goals.sortedBy { it.category }.forEach {
+            if (categoriesMap[it.category] == false && it.category != Category.NOT_SET.id) {
+                this.items.add(GoalUi(it, true))
+                this.items.add(GoalUi(it, false))
+                categoriesMap[it.category] = true
+            } else {
+                this.items.add(GoalUi(it, false))
+            }
+        }
         this.notifyDataSetChanged()
     }
 
@@ -62,7 +97,18 @@ internal class GoalsAdapter(
     //endregion
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    class GoalsViewHolder(
+    inner class GoalsViewHolder(
         val binding: GoalsItemGoalBinding
     ) : RecyclerView.ViewHolder(binding.root)
+
+    inner class HeaderViewHolder(
+        val binding: GoalsItemHeaderBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+
+    data class GoalUi(val goal: IGoal, val isHeader: Boolean) : IGoal by goal
+
+    companion object {
+        private const val VIEW_TYPE_HEADER = 0
+        private const val VIEW_TYPE_GOAL = 1
+    }
 }
