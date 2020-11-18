761509f6820a01e5634ce7d6f166909e1344f957
Viktor Apoyan <chamich.apps@gmail.com>
2020-11-18 20:35:25 +0100

    Glide dependency added, Avatar image is loaded
    Signed-off-by: Viktor Apoyan <chamich.apps@gmail.com>

--- features/profile/src/main/java/app/chamich/feature/profile/ui/menu/ProfileMenuBottomSheet.kt
+++ features/profile/src/main/java/app/chamich/feature/profile/ui/menu/ProfileMenuBottomSheet.kt
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatDelegate
 import app.chamich.feature.profile.R
 import app.chamich.feature.profile.databinding.ProfileBottomSheetProfileMenuBinding
 import app.chamich.library.core.CoreBottomSheetDialogFragment
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -75,6 +76,13 @@ internal class ProfileMenuBottomSheet :
         val user = viewModel.getCurrentUser()
         binding.textViewUsername.text = user.displayName
         binding.textViewEmail.text = user.email
+
+        Glide
+            .with(this)
+            .load(user.photoUrl)
+            .centerCrop()
+            .placeholder(R.drawable.profile_avatar_placeholder)
+            .into(binding.imageViewProfileAvatar)
     }
 
     private fun initializeToggleButton() {
